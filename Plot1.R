592a25bc9f63f17e993dfd6bceb346f726e92efc
SayakaT-tky <sayakatanaka-tky@umin.net>
2017-06-04 21:35:18 +0900

    CourseProject1_Plot1

--- Plot1.R
+++ Plot1.R
@@ -2,6 +2,8 @@
 ##Exploratory Data Anlysis Course Project-1
 ##Plot1.R
 
+Sys.setlocale("LC_TIME", "English")
+
 File<-"./household_power_consumption.txt"
 Data1<-read.table(File,header=T,sep=";",na.strings="?",stringsAsFactors=F)
 Data1<-subset(Data1,Date %in% c("1/2/2007","2/2/2007"))
